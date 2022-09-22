58205eb01c3b4c1df697cb56f66006ecabd7bc3b
luca-ros <lucattparte@gmail.com>
2022-09-22 09:26:56 +0200

    fix: urlFor import

--- pages/index.tsx
+++ pages/index.tsx
@@ -36,9 +36,9 @@ export default function Home({ posts }: Props) {
         {posts.map(post => (
           <Link key={post._id} href={`/post/${post.slug.current}`} >
             <div className='border rounded-lg group cursor-pointer overflow-hidden'>
-              <img className='h-60 w-full object-cover group-hover:scale-105 transition-transform duration-200 ease-in-out' 
-              src={
-                urlFor(post.mainImage).url()!} alt="" />
+              <img className='h-60 w-full object-cover group-hover:scale-105 transition-transform duration-200 ease-in-out'
+                src={
+                  urlFor(post.mainImage).url()!} alt="" />
               <div className='flex justify-between p-5 bg-white'>
                 <div>
                   <p className='text-lg font-bold'>{post.title}</p>
--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -2,6 +2,7 @@ import { GetStaticProps } from "next";
 import Header from "../../components/Header";
 import { sanityClient, urlFor } from "../../sanity";
 import { Post } from "../../typings";
+import PortableText from "react-portable-text";
 
 interface Props {
     post: Post;
@@ -11,6 +12,55 @@ function Post({ post }: Props) {
     return (
         <main>
             <Header />
+
+            <img
+                className="w-full h-40 object-cover"
+                src={urlFor(post.mainImage).url()!}
+                alt=""
+            />
+
+            <article className="max-w-3xl mx-auto p-5">
+                <h1 className="text-3xl mt-10 mb-3">{post.title}</h1>
+                <h2 className="text-xl font-light text-gray-500 mb-2" >{post.description}</h2>
+
+                <div className="flex items-center space-x-2">
+                    <img
+                        className="h-10 w-10 rounded-full"
+                        src={urlFor(post.author.image).url()!}
+                        alt=""
+                    />
+                    <p className="font-extralight text-sm">
+                        By {" "}
+                        <span className="text-orange-400 font-semibold">{post.author.name} - </span>
+
+                        Published at {new Date(post._createdAt).toLocaleString()}
+                    </p>
+                </div>
+
+                <div>
+                    <PortableText
+                        className=""
+                        dataset={process.env.NEXT_PUBLIC_SANITY_DATASET!}
+                        projectId={process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!}
+                        content={post.body}
+                        serializers={{
+                            h1: (props: any) => (
+                                <h1 className="text-2xl font-bold my-5" {...props} />
+                            ),
+                            h2: (props: any) => (
+                                <h2 className="text-xl font-bold my-5" {...props} />
+                            ),
+                            li: ({ children }: any) => (
+                                <li className="ml-4 list-disc"{children}> </li>
+                            ),
+                            link: ({ href, children }: any) => (
+                                <a href={href} className="text-blue-500 hover:underline" {children} />
+                            ),
+                        }}
+                    />
+                </div>
+
+            </article>
         </main>
     )
 }
@@ -70,6 +120,7 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
     return {
         props: {
             post,
-        }
-    }
-}
+        },
+        revalidate: 600, // 600sec automatic update the old cache
+    };
+};
